497608f93ac69b169b47b68791e2d54709ff7563
matt <matt.rea@gmail.com>
2017-06-17 17:53:36 -0500

    initial commit

---
+++ src/questions.js
@@ -0,0 +1,323 @@
+'use strict';
+
+module.exports = {
+    /**
+     * When editing your questions pay attention to your punctuation. Make sure you use question marks or periods.
+     * Make sure the first answer is the correct one. Set at least ANSWER_COUNT answers, any extras will be shuffled in.
+     */
+    "QUESTIONS_EN_US" : [
+      {
+          "This pandemic resulted in the deaths of over one third of Europes population in the 14th century": [
+              "Black Death / Bubonic Plague",
+              "Cholera",
+              "Smallpox",
+              "Leprosy",
+              "Zika Virus"
+          ]
+      },
+      {
+          "The American Civil War spaned between what years?": [
+              "1861 to 1865",
+              "1775 to 1783",
+              "1941 to 1945",
+              "1492 to 1495"
+          ]
+      },
+      {
+          "What baseball player holds the record for the most consecutive games played?": [
+              "Cal Ripken, Jr.",
+              "Willie Mays",
+              "Ken Griffey Jr.",
+              "Billy Ripken"
+          ]
+      },
+      {
+          "How many holes are played in a standard round of golf": [
+              "18",
+              "9",
+              "20",
+              "11"
+          ]
+      },
+      {
+          "What NFL team has the only perfect season?": [
+              "Miami Dolphins",
+              "New England Patriots",
+              "Chicago Bears",
+              "Dallas Cowboys"
+          ]
+      },
+      {
+          "What US president presided over most of World War two?": [
+              "Franklin Roosevelt",
+              "Dwight Eisenhower",
+              "Herbert Hoover",
+              "Woodrow Wilson"
+          ]
+      },
+      {
+          "What is NOT one of the three branches of federal government?": [
+              "Military",
+              "Executive",
+              "Judicial",
+              "Legislative"
+          ]
+      },
+      {
+          "In mathematical terms, what does PI represent?": [
+              "The ratio of a circles circumference to its diameter",
+              "The speed of light in a black hole",
+              "The ratio of a squares perimeter to its area",
+              "The ration of a cylinders volume to its height"
+          ]
+      },
+      {
+          "What do you call the top portion of a fraction?": [
+              "Numerator",
+              "Denominator",
+              "Product",
+              "Upper Limit"
+          ]
+      },
+      {
+          "A triangle has interior angles that add up to what?": [
+              "180 degrees",
+              "90 degrees",
+              "360 degrees",
+              "270 degrees"
+          ]
+      },
+      {
+          "An improper fraction is always greater than what number?": [
+              "1",
+              "2",
+              "100",
+              "10"
+          ]
+      },
+      {
+          "How many pairs of chromosomes does a human have?": [
+              "23",
+              "100",
+              "46",
+              "32"
+          ]
+      },
+      {
+          "He was the first US treasury secretary, and famously died in a duel with Vice President Aaron Burr": [
+              "Alexander Hamilton",
+              "Thomas Jefferson",
+              "Thomas Paine",
+              "John Adams"
+          ]
+      },
+      {
+          "How many miles away is the earth from the sun?": [
+              "93 million",
+              "32 million",
+              "93 thousand",
+              "39 billion"
+          ]
+      },
+      {
+          "What is the name of the nearest galaxy to the Milky Way?": [
+              "Andromeda",
+              "Black Eye",
+              "Alpha Orion",
+              "Pegasus"
+          ]
+      },
+      {
+          "What is the approximate population of earth?": [
+              "7 billion",
+              "10 billion",
+              "5 billion",
+              "300 billion"
+          ]
+      },
+      {
+          "What is the approximate population of the United States?": [
+              "320 million",
+              "500 million",
+              "100 million",
+              "200 million"
+          ]
+      },
+      {
+          "The House of representatives and The Senate make up the two houses of what body?": [
+              "Congress",
+              "Electoral College",
+              "The Supreme Court",
+              "Parliment"
+          ]
+      },
+      {
+          "US senators are given a term of how many years?": [
+              "6",
+              "4",
+              "2",
+              "8"
+          ]
+      },
+      {
+          "US House representatives are given a term of how many years?": [
+              "2",
+              "4",
+              "6",
+              "8"
+          ]
+      },
+      {
+          "The Supreme Court is comprised of how many justices?": [
+              "9",
+              "11",
+              "50",
+              "8"
+          ]
+      },
+      {
+          "A kilometer equals about how many miles?": [
+              "0.6 miles",
+              "2.2 miles",
+              "5 miles",
+              "1.6 miles"
+          ]
+      },
+      {
+          "A kilogram equals about how many pounds?": [
+              "2.2 pounds",
+              "1.2 pounds",
+              "5 pounds",
+              "0.6 pounds"
+          ]
+      },
+      {
+          "What basesball team has won the most world series?": [
+              "New York Yankees",
+              "St. Louis Cardinals",
+              "Los Angeles Dodgers",
+              "Boston Red Sox"
+          ]
+      },
+      {
+          "On average how much does a new car cost?": [
+              "$30,000",
+              "$10,000",
+              "$7,000",
+              "$20,000"
+          ]
+      },
+      {
+          "This author wrote about the Adventures of Tom Sawyer and Huckleberry Finn.": [
+              "Mark Twain",
+              "Charles Dickens",
+              "Ernest Hemingway",
+              "William Shakespeare"
+          ]
+      },
+      {
+          "This writer is best known for his poems and plays, including Hamlet, Macbeth, and Romeo and Juliet.": [
+              "William Shakespeare",
+              "Christopher Marlowe",
+              "Mark Twain",
+              "Ernest Hemingway"
+          ]
+      },
+      {
+          "This American writer penned such classics as The Grapes of Wrath, and Of Mice and Men.": [
+              "John Steinbeck",
+              "Ernest Hemingway",
+              "F. Scott Fitzgerald",
+              "William Faulkner"
+          ]
+      },
+      {
+          "This young writer was forced to go into hiding during the Holocaust and journaled her experiences in a famous diary.": [
+              "Anne Frank",
+              "Emily Brontë",
+              "Agatha Christie",
+              "J.K. Rowling"
+          ]
+      },
+      {
+          "This famous civil rights leader was best know for non-violent, acts of civil disobedience based on his Christian beliefs.": [
+              "Martin Luther King Jr",
+              "Malcolm X",
+              "Rosa Parks",
+              "Lyndon Johnson"
+          ]
+      },
+      {
+          "This famous civil rights figure was famous for refusing to give up her seat on the bus to a white person.": [
+              "Rosa Parks",
+              "Coretta Scott King",
+              "Daisy Bates",
+              "Annie Devine"
+          ]
+      },
+      {
+          "What is the definition of a prime number?": [
+              "A whole number greater than 1 that can ONLY be divided by itself and 1",
+              "A irrational number that can never be calculated",
+              "A number that ends in a 7",
+              "The last number in the powerball drawing"
+          ]
+      },
+      {
+          "During World War Two, what three countries were labeled as the Axis powers?": [
+              "Germany, Italy, and Japan",
+              "United States, France, and Great Britain",
+              "United States, France, and Soviet Union",
+              "Germany, United States, and Australia"
+          ]
+      },
+      {
+          "During World War Two, which of the following countries were NOT an Allied power?": [
+              "Japan",
+              "United States",
+              "Great Britain",
+              "The Soviet Union"
+          ]
+      },
+      {
+          "This period of European history followed the Dark Ages and is best know for a renewed focus on arts, science, and literature.": [
+              "The Renaissance",
+              "Medieval Ages",
+              "The Reformation",
+              "The Age of Enlightenment"
+          ]
+      },
+      {
+          "This bands arrival in America in 1964 marked the beginning of the phenomenon known as the British Invasion": [
+              "The Beatles",
+              "Coldplay",
+              "The Beach Boys",
+              "Queen"
+          ]
+      },
+      {
+          "Which of the following is NOT a style of Greek columns?": [
+              "Tuscan",
+              "Doric",
+              "Ionic",
+              "Corinthian"
+          ]
+      },
+      {
+          "The laws of supply and demand determines that when something is in short supply, its price will do what? ": [
+              "Go up",
+              "Go down",
+              "Stay the same",
+              "Vary based on the weather"
+          ]
+      },
+      {
+          "What is the term when a single company is the only seller of a particluar product or commodity?": [
+              "Monopoly",
+              "Oligopoly",
+              "Free market",
+              "Applied market"
+          ]
+      }
+    ]
+};
